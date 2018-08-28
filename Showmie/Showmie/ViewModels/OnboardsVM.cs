c012b972b2bed99f95e0688d439f1f8d06cb10a0
Peter Uchenna Esenwa <bigjove1@yahoo.com>
2018-08-28 02:23:45 +0100

    Rounding up UI for the Onboarding screens

--- Showmie/Showmie/ViewModels/OnboardsVM.cs
+++ Showmie/Showmie/ViewModels/OnboardsVM.cs
@@ -4,7 +4,7 @@ namespace Showmie
 {
     internal class OnboardsVM
     {
-        public SingleBoard thirdBoard = new SingleBoard("light_bulb.png", "View the works of many other professionals from around the continient and get inspired", "Get Inspired to Create");
+        public SingleBoard thirdBoard = new SingleBoard("idea_bulb.png", "View the works of many other professionals from around the continient and get inspired", "Get Inspired to Create");
         public SingleBoard secondBoard = new SingleBoard("tailor_showoff.png", "Share content from your gallery our show some of your recent designs", "Showcase your works");
         public SingleBoard firstBoard = new SingleBoard("sm_community", "Connect with lots of fashion lovers and professionals in and outside the country", "A Thriving Fashion Community");
 
