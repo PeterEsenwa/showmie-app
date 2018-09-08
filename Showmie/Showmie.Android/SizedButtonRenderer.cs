fa0d2ff0871735a67813667896b005e20d7c2ce1
Peter Uchenna Esenwa <bigjove1@yahoo.com>
2018-09-08 16:06:49 +0100

    Finalizing Signup page UI

---
+++ Showmie/Showmie.Android/SizedButtonRenderer.cs
@@ -0,0 +1,43 @@
+﻿using Android.Content;
+using Showmie.Droid;
+using Showmie.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Android.Support.V7.Widget;
+using ButtonRenderer = Xamarin.Forms.Platform.Android.AppCompat.ButtonRenderer;
+
+[assembly: ExportRenderer(typeof(SizedButton), typeof(SizedButtonRenderer))]
+namespace Showmie.Droid
+{
+    class SizedButtonRenderer: ButtonRenderer
+    {
+        private SizedButton myButton;
+        private AppCompatButton androidButton;
+        public SizedButtonRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement != null)
+                myButton = e.OldElement as SizedButton;
+            if (e.NewElement != null)
+                myButton = e.NewElement as SizedButton;
+            if (Element != null)
+                myButton = Element as SizedButton;
+
+            if (Control != null)
+            {
+                androidButton = Control as AppCompatButton;
+            }
+            else
+            {
+                androidButton = CreateNativeControl();
+                SetNativeControl(androidButton);
+            }
+
+            androidButton.SetPadding((int)myButton.Padding.Left, (int)myButton.Padding.Top, (int)myButton.Padding.Right, (int)myButton.Padding.Bottom);
+        }
+    }
+}
--- Showmie/Showmie/Views/SizedButton.cs
+++ Showmie/Showmie/Views/SizedButton.cs
@@ -7,25 +7,18 @@ namespace Showmie.Views
 {
     class SizedButton : Button
     {
-        public string Padding {
-            get { return (string)GetValue(PaddingProperty); }
+        public Thickness Padding {
+            get { return (Thickness)GetValue(PaddingProperty); }
             set { SetValue(PaddingProperty, value); }
         }
         public static BindableProperty PaddingProperty { get; } = BindableProperty.Create(
           "Padding",
-          typeof(string),
+          typeof(Thickness),
           typeof(SizedButton),
-          "8",
+          new Thickness(5),
           propertyChanged: (bindable, oldvalue, newvalue) =>
           {
               ((SizedButton)bindable).InvalidateMeasure();
           });
-        protected override SizeRequest OnMeasure(double widthConstraint, double heightConstraint)
-        {
-            SizeRequest sizeRequest = Measure(double.PositiveInfinity, double.PositiveInfinity, MeasureFlags.IncludeMargins);
-            sizeRequest = new SizeRequest(new Size(width: sizeRequest.Request.Width + int.Parse(Padding), height: sizeRequest.Request.Height));
-            return sizeRequest;
-        }
- 
     }
 }
